627d3831ff9dcd4b88157ef70b7e48157664945b
Juanjosecanog <juanjosecanogiraldo5@gmail.com>
2025-04-01 19:33:16 -0500

    creacion de clase Node

--- Ejercicio#1.py
+++ Ejercicio#1.py
@@ -4,5 +4,12 @@ import json
 with open("arbol_diagnostico_medico.json", "r",encoding="utf-8") as archivo:
     arbol_diagnostico = json.load(archivo)
 
-# Verificar que el JSON se ha cargado correctamente
-print(arbol_diagnostico)
+class Node:
+    def __init__(self, pregunta: str | None = None, diagnostico: str | None = None):
+        self.pregunta: str | None = pregunta
+        self.si = Node | None  #sub abrol izqueirdo
+        self.no = Node | None  #sub arbol derecho
+        self.diagnostico: str | None = diagnostico
+
+
+
