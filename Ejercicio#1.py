72246f06d5a13d14fcd2a97030438a313e064aab
Juanjosecanog <juanjosecanogiraldo5@gmail.com>
2025-04-01 22:03:22 -0500

    recorrido del arbol

--- Ejercicio#1.py
+++ Ejercicio#1.py
@@ -15,22 +15,51 @@ class Node:
         return self.diagnostico is not None and self.left is None and self.right is None
 
 class DecisionTreeNode:
-    def __init__(self, data: str, left=None, right=None):
-        self.data = data
-        self.left = left
-        self.right = right
+    def __init__(self, root: Node | None = None):
+        """
+                El árbol de decisiones.
+
+                :param root: Nodo raíz del árbol de decisiones (es el punto de partida).
+                """
+        self.root: Node | None = root
 
     def recorrer_arbol(self, node: Node):
-        # Si el nodo es None, no hacemos nada (caso base)
+        """
+                Función recursiva para recorrer el árbol de decisiones y hacer preguntas al usuario hasta llegar a un diagnóstico.
+
+                :param node: Nodo actual del árbol. Si no se pasa, comenzamos desde la raíz.
+                """
         if node is None:
-            return
+            node = self.root
+
         # Si el nodo tiene un diagnóstico, mostramos el diagnóstico
-        if node.left is None and node.right is None:
-            print(f"Diagnostico {self.data}")
+        if node.is_leaf():
+            print(f"diagnostico, {node.diagnostico}")
             return
 
-        print(node.pregunta)
+        print(f"pregunta, {node.pregunta}")
         respuesta = input("¿Respuesta (sí/no)? ").strip().lower()
+
+        if respuesta == "si":
+            self.recorrer_arbol(node.left)
+        elif respuesta == "no":
+            self.recorrer_arbol(node.right)
+        else:
+            print("Respuesta no valida")
+
+if __name__ == "__main__":
+    # Cargar el árbol desde el archivo JSON
+    with open("arbol_diagnostico_medico.json", "r", encoding="utf-8") as archivo:
+        arbol_diagnostico = json.load(archivo)
+
+    # Crear el árbol de decisiones
+    root = Node(pregunta=arbol_diagnostico["pregunta"])
+    decision_tree = DecisionTreeNode(root=root)
+
+    # Ejecutar el recorrido del árbol
+    decision_tree.recorrer_arbol(decision_tree.root)
+
+
 
 
 
